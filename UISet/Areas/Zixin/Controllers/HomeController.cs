449c020eb777d0406709d354c15cf887a9f6e81f
FrankFrankWei <yanzhifei25@163.com>
2019-04-19 14:31:24 +0800

    move independent projects(apple and zixin ui demo) to areas in new project UISet

---
+++ UISet/Areas/Zixin/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UISet.Areas.Zixin.Controllers
+{
+    [Area("Zixin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Products()
+        {
+            return View();
+        }
+
+        public IActionResult Customs()
+        {
+            return View();
+        }
+
+        public IActionResult News()
+        {
+            return View();
+        }
+
+        public IActionResult Team()
+        {
+            return View();
+        }
+
+        public IActionResult Companies()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            //return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return null;
+        }
+    }
+}
